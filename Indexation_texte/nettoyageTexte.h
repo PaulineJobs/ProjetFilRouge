868cc63a5ad0f792b9aa5e55c10d51082e5e7c30
Manooon <92680110+ManonCouffin@users.noreply.github.com>
2021-12-03 09:33:56 +0100

    Premiere partie de l'indexation texte : nettoyage du texte

---
+++ Indexation_texte/nettoyageTexte.h
@@ -0,0 +1,21 @@
+#ifndef ELEMENT_H 
+#define ELEMENT_H
+
+#include <stdio.h>
+#include <dirent.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+void passageXmlClean();
+
+void majMin();
+
+void suppressionBalise();
+
+void espaceApostrophe();
+
+void Nettoyage();
+
+#endif
